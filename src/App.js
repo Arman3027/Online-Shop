f9cc863d4c98ca52f0d726fe6c7af664f0911132
Arman3027 <solaa8239@gmail.com>
2024-02-18 11:33:28 +0330

    products component added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Route, Routes, Navigate } from "react-router-dom"
+import { useState, useEffect  } from "react";
 import ProductContext from "./context/context";
 import Products from "./component/products";
 import About from "./component/about";
@@ -6,25 +7,157 @@ import Navbar from './component/navbar'
 import Login from "./component/login";
 import Home from "./component/home";
 import "./style/style.css";
+import axios from "axios";
 
 const App = () => {
+  const [itemProducts, setProducts] = useState([]);
+  const [countitem, setcount] = useState(0);
+  const [cartitem, setcartitem] = useState([])
+  const [iscart, setiscart] = useState(false)
+  const [totalprice, settotal] = useState(0)
+  const [filteritem, setfilteritem] = useState([])
+
+  useEffect(() => {
+    async function getItem() {
+      let response = await axios.get("https://fakestoreapi.com/products");
+      setProducts(response.data);
+      setfilteritem(response.data)
+    }
+    getItem();
+  }, []);
+
   return (
     <>
-      <ProductContext.Provider value={{}}>
-          <Navbar />
-          <Routes>
-            <Route path="/products" element={<Products />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/not-found" element={<h1>not found ¯\_ (¬_¬)_/¯</h1>} />
-            <Route path="/home" element={<Navigate to="/" replace />} />
-            <Route path="/" element={<Home />} />
-            <Route path="*" element={<Navigate to="/not-found" replace />} />
-          </Routes>
-      
+      <ProductContext.Provider
+        value={{
+          itemProducts: itemProducts,
+          onbuy: handlebuy,
+          onplus: handleplus,
+          onmines: handlemines,
+          onsubmit: handlesubmit,
+          onclick: handleactive,
+          ontotal: handletotal,
+          ondelete: handledelete,
+          onchange: handlechange,
+          ongroup: handlegroup,
+          countitem: countitem,
+          cartitem: cartitem,
+          iscart: iscart,
+          totalprice: totalprice,
+          filteritem:filteritem,
+        }}
+      >
+        <Navbar />
+        <Routes>
+          <Route path="/products" element={<Products />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/not-found" element={<h1>not found ¯\_ (¬_¬)_/¯</h1>} />
+          <Route path="/home" element={<Navigate to="/" replace />} />
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<Navigate to="/not-found" replace />} />
+        </Routes>
       </ProductContext.Provider>
     </>
   );
+  function handlebuy(buy_products, cart_procuts, product, target) {
+    let allbuy = buy_products.current
+    let allcart = cart_procuts.current
+    let arrallbuy = [...Object.values(allbuy)];
+    let arrallcart = [...Object.values(allcart)]
+    let brother = target.parentElement.childNodes[3]
+    arrallbuy.forEach((element) => {
+      if (element !== null) { element.style.display = 'unset' }
+    })
+    arrallcart.forEach((element) => {
+      if (element !== null) { element.style.display = 'none' }
+    })
+    target.style.display = 'none'
+    brother.style.display = 'flex'
+    setcount(0);
+  }
+  function handleplus() {
+    let newcount = countitem + 1
+    setcount(newcount)
+    return countitem
+  }
+  function handlemines() {
+    if (countitem > 0) {
+      let newcount = countitem - 1;
+      setcount(newcount);
+      return countitem;
+    }
+  }
+  function handlesubmit(buy_products, cart_procuts, product,target) {
+    let index = product.id - 1;
+    let brother = target.parentElement.parentElement.childNodes[4]
+    brother.style.display = "unset";
+    target.parentElement.style.display = "none";
+
+    let item = {
+      title: product.title,
+      count: countitem,
+      price: product.price,
+      allprice: product.price * countitem,
+    }
+    if (item.allprice !== 0) {
+      cartitem.push(item)
+      setcartitem(cartitem)
+    }
+    setcount(0)
+  }
+  function handleactive() {
+    if (iscart === false) {
+      setiscart(true)
+    } else {
+      setiscart(false)
+    }
+  }
+  function handledelete(item) {
+    let index = cartitem.indexOf(item)
+    if (index > -1) {
+      cartitem.splice(index , 1)
+    }
+    setcartitem(cartitem)
+  }
+  function handletotal() {
+    let newtotal=0
+    cartitem.forEach((item) => {
+      newtotal += item.allprice;
+    })
+    settotal(newtotal)
+  }
+  function handlechange(value, select) {
+    let newfilteritem;
+    if (select.value === 'All') {
+      newfilteritem = itemProducts.filter((item) => item.title.toString().toLowerCase().includes(value.toLowerCase()))
+      setfilteritem(newfilteritem)
+    } else if (select.value === "electronics") {
+      newfilteritem = itemProducts.filter((item) => item.title.toString().toLowerCase().includes(value.toLowerCase()) && item.category === 'electronics')
+      setfilteritem(newfilteritem)
+    } else if (select.value === "jewelery") {
+      newfilteritem = itemProducts.filter((item) => item.title.toString().toLowerCase().includes(value.toLowerCase()) && item.category === 'jewelery')
+      setfilteritem(newfilteritem)
+    } else if (select.value === "men's clothing") {
+      newfilteritem = itemProducts.filter((item) => item.title.toString().toLowerCase().includes(value.toLowerCase()) && item.category === "men's clothing")
+      setfilteritem(newfilteritem)
+    } else if (select.value === "women's clothing") {
+      newfilteritem = itemProducts.filter((item) => item.title.toString().toLowerCase().includes(value.toLowerCase()) && item.category === "women's clothing")
+      setfilteritem(newfilteritem)
+    }
+  }
+  function handlegroup(value,search) {
+    search.value = ''
+    let newfilteritem = itemProducts.filter((item) => {
+      if (value === 'All') {
+        return itemProducts
+      } else {
+        return item.category === value;
+      }
+    })
+    setfilteritem(newfilteritem)
+  }
 }
+
  
 export default App;
--- src/component/products.jsx
+++ src/component/products.jsx
@@ -1,7 +1,173 @@
 import "../style/style.css";
+import { useState , useEffect , useRef } from "react";
+import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import { faCircleXmark } from "@fortawesome/free-regular-svg-icons";
+import { useContext } from "react";
+import ProductContext from "../context/context";
+
 
 const Products = () => {
-  return <h1>Products</h1>;
+  const productcontext = useContext(ProductContext)
+  let cart_products = useRef({})
+  let buy_products = useRef({});
+  let search_products = useRef(null)
+  let select_products = useRef(null)
+  
+  return (
+    <>
+      <div className="container-products">
+        <div className="grid-products">
+          <input
+            type="text"
+            id="search"
+            placeholder="search"
+            onChange={(e) => {
+              handlechange(e.target.value,select_products.current);
+            }}
+            ref={search_products}
+          />
+          <select
+            ref={select_products}
+            className="select-products"
+            onChange={(e) => {
+              handlegroup(e.target.value , search_products.current);
+            }}
+          >
+            <option value="All">All</option>
+            <option value="electronics">electronics</option>
+            <option value="jewelery">jewelery</option>
+            <option value="men's clothing">men's clothing</option>
+            <option value="women's clothing">women's clothing</option>
+          </select>
+          <FontAwesomeIcon
+            icon={faCartShopping}
+            className="faCart-products"
+            onClick={handleactive}
+          />
+          <div
+            className="cartpage-products"
+            style={{ display: productcontext.iscart ? "flex" : "none" }}
+          >
+            <FontAwesomeIcon
+              icon={faCircleXmark}
+              className="faCircle-products"
+              onClick={handleactive}
+            />
+            <div className="infoinfo-products">
+              <p className="topinfo-products">title</p>
+              <p className="topinfo-products">count</p>
+              <p className="topinfo-products" id="topcartprice-products">price</p>
+              <p className="topinfo-products">total price</p>
+            </div>
+            <div className="infobuy-products">
+              {productcontext.cartitem.length > 0 ? (
+                productcontext.cartitem.map((item) => {
+                  return (
+                    <>
+                      <div className="cartitem-products">
+                        <p className="itemtitle-products">{item.title}</p>
+                        <p className="itemcartitem-products">{item.count}</p>
+                        <p className="itemcartitem-products" id="cartprice-products">{item.price}</p>
+                        <p className="itemcartitem-products">{item.allprice}</p>
+                        <button
+                          className="deletecartitem-products"
+                          onClick={() => {
+                            handledelete(item);
+                          }}
+                        >
+                          delete
+                        </button>
+                      </div>
+                    </>
+                  );
+                })
+              ) : (
+                <h4 style={{ margin: 40 }}>your shopping basket is empty</h4>
+              )}
+            </div>
+            <div className="cartprice-products">
+              price :{" "}
+              {productcontext.totalprice === 0
+                ? "zero"
+                : productcontext.totalprice}
+            </div>
+          </div>
+          {productcontext.filteritem.map((product, index) => {
+            return (
+              <>
+                <div className="item-products" key={index}>
+                  <img src={product.image} className="img-products" />
+                  <p className="price-products">{product.price}$</p>
+                  <p className="title-products">{product.title}</p>
+                  <div
+                    className="cart-products"
+                    ref={(element) => (cart_products.current[index] = element)}
+                  >
+                    <button
+                      onClick={(e) => {
+                        handlesubmit(product, e.currentTarget);
+                      }}
+                      className="submit-products"
+                    >
+                      submit
+                    </button>
+                    <p className="count-products">{productcontext.countitem}</p>
+                    <button className="plus-products" onClick={handleplus}>
+                      +
+                    </button>
+                    <button className="mines-products" onClick={handlemines}>
+                      -
+                    </button>
+                  </div>
+                  <button
+                    onClick={(e) => {
+                      handlebuy(product, e.currentTarget);
+                    }}
+                    className="buy-products"
+                    ref={(element) => (buy_products.current[index] = element)}
+                  >
+                    buy
+                  </button>
+                </div>
+              </>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+       
+  function handlebuy(product ,target) {
+    productcontext.onbuy(buy_products, cart_products, product,target);
+  }
+  function handleplus() {
+    productcontext.onplus()
+  }
+  function handlemines() {
+    productcontext.onmines();
+  }
+  function handlesubmit(product,target) {
+    productcontext.onsubmit(buy_products, cart_products, product,target);
+    handletotal()
+  }
+  function handleactive() {
+    productcontext.onclick()
+  }
+  function handledelete(item) {
+    productcontext.ondelete(item)
+    handletotal()
+  }
+  function handletotal() {
+    productcontext.ontotal()
+  }
+  function handlechange(value ,select) {
+    productcontext.onchange(value,select)
+  }
+  function handlegroup(value ,search) {
+    productcontext.ongroup(value,search)
+  }
 };
 
 export default Products;
