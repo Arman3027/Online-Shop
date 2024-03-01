0473b755ef9259f99b5ad38ffe80dc74f7517555
Arman3027 <solaa8239@gmail.com>
2024-03-01 12:02:49 +0330

    about page added

--- src/component/about.jsx
+++ src/component/about.jsx
@@ -3,7 +3,7 @@ import "../style/style.css";
 const About = () => {
   return <>
     <div className="container-about">
-      <div className="flex-container">
+      <div className="flex-about">
         <p>developer : Arman solaymani</p>
         <p>email : solaa8239@gamil.com</p>
         <p>number : 0910-746-0590</p>
